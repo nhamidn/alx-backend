2fdc8e0dcd21c5409ffe5dc5c7d98ef23b55e1d3
nhamidn <nhamid@student.1337.ma>
2023-12-27 18:21:35 +0100

    0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,4 +6,6 @@
 
 def index_range(page, page_size):
     """Function that return a paginated pages range"""
-    pass
+    last_element = page * page_size
+    first_element = last_element - page_size
+    return (first_element, last_element)
