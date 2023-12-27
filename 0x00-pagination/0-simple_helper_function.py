beb07c67a127354605be3a7e9326589e48b35396
nhamidn <nhamid@student.1337.ma>
2023-12-27 18:32:46 +0100

    documentation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-0-simple_helper_function module
+0-simple_helper_function module that defines a index_range function.
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple:
-    """Function that return a paginated pages range"""
+    """Function that return a paginated pages range."""
     last_element = page * page_size
     first_element = last_element - page_size
     return (first_element, last_element)
