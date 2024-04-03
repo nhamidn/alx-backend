34a5dfba309118f34f93069bc463a94508e037a1
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-03 14:21:31 +0000

    2-lifo_cache

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" Basic-Cache module
+"""
+
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ A simple LIFO caching system.
+    """
+
+    def __init__(self):
+        """ Init method.
+        """
+        super().__init__()
+        self.key_order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache data.
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                if key in self.key_order:
+                    self.key_order.remove(key)
+                self.key_order.append(key)
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    discarded_key = self.key_order.pop()
+                    del self.cache_data[discarded_key]
+                    print("DISCARD: {}".format(discarded_key))
+                self.cache_data[key] = item
+                self.key_order.append(key)
+
+    def get(self, key):
+        """ Get an item by key from cache data.
+        """
+        return self.cache_data.get(key, None)
