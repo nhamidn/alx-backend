3bb3a5ebfcb3120b08245178180edf6b2eb9c418
Noureddine hamid <hamid-savana@hotmail.fr>
2024-05-09 00:45:48 +0100

    Create the Job processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -4,7 +4,7 @@ const queue = createQueue({name: 'push_notification_code'});
 
 const jobData = {
   phoneNumber: '0643526182',
-  message: 'This is a notification message.',
+  message: 'This is the code to verify your account',
 };
 
 const job = queue.create('push_notification_code', jobData);
